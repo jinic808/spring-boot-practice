c4fe9ba6bac7d1da25da996761787fc15fcec8be
jinic808 <kimjini9030@gmail.com>
2023-08-24 11:04:21 -0700

    Added Student class

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,8 +1,14 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -10,6 +16,13 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping
+	public List<Student> student() {
+		return List.of(
+				new Student(0L, "Myeongnam", "myeongnam@gmail.com", LocalDate.of(1962, Month.SEPTEMBER, 13), 60)
+		);
 	}
 
 //	@GetMapping
