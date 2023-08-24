c41c64b111e73c542be3ad51c300d018f7353de1
jinic808 <kimjini9030@gmail.com>
2023-08-24 10:27:33 -0700

    Commented out DB in pom and added examples

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,24 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+//	@GetMapping
+//	public List<String> hello() {
+//		return List.of("Hello", "World");  // will return JSON ["Hello","World"]
+//	}
+
+//	@GetMapping
+//	public String hello() {
+//		return "Hello World";  // will get String "Hello World"
+//	}
 
 }
